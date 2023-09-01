402dc9e544826fa6a6a81d734f29c89cbb191c49
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-09-01 22:00:11 +0530

    Add files via upload

--- pizza-controller.js
+++ pizza-controller.js
@@ -2,7 +2,7 @@ import pizzaOperations from "./pizza-operations.js";
 async function printPizzas(){
     const allPizzas = await pizzaOperations.getPizzas();
     const div = document.getElementById('pizza-output');
-    console.log('All Pizza ', allPizzas);
+    // console.log('All Pizza ', allPizzas);
     for(var pizza of allPizzas){
         const card = createCard(pizza);
         div.appendChild(card);
@@ -18,25 +18,53 @@ function printBasket(){
     const basketDiv = document.getElementById('basket');
     basketDiv.innerHTML = '';
     const pizzasInCart = pizzaOperations.pizzas.filter(pizza=>pizza.isAddedInCart);
+    while (basketDiv.firstChild) {
+        basketDiv.removeChild(basketDiv.firstChild);
+    }
+
+    // Check if basket heading is already added
+    const basketHeading = basketDiv.querySelector('.basket-heading');
+    if (!basketHeading) {
+        // Add the basket heading and icon
+        const newBasketHeading = document.createElement('div');
+        newBasketHeading.className = 'basket-heading';
+        newBasketHeading.innerHTML = `<h2 class="mr-2">Your Basket <hr> </h2>`;
+        basketDiv.prepend(newBasketHeading); // Add the heading at the beginning
+    }
     pizzasInCart.forEach(pizza=>{
         const pTag = printItem(pizza);
         basketDiv.appendChild(pTag);
     });
+
     const total = printTotal(pizzasInCart);
-    // console.log('Total is ', total);
-    const afterGST = total * 1.18;
     const totalPTag = document.createElement("p");
-    totalPTag.innerText = `Total is ${total.toFixed(2)}`;
+    totalPTag.innerText = `Subtotal  ₹${total.toFixed(2)}`;
     basketDiv.appendChild(totalPTag);
 
+    const afterTax = total * 0.05;
+    const totalTax = document.createElement("p");
+    totalTax.innerText = `Total Tax  ₹${afterTax.toFixed(2)}`;
+    basketDiv.appendChild(totalTax);
+
+    const afterGST = total * 1.05;
     const totalGSTPTag = document.createElement("p");
-    totalGSTPTag.innerText = `Total after applicable GST is ${afterGST.toFixed(2)}`;
+    totalGSTPTag.innerText = `Amount Payable  ₹${afterGST.toFixed(2)}`;
+    totalGSTPTag.style = "color:green";
     basketDiv.appendChild(totalGSTPTag);
+    
+    const payButtonContainer = document.getElementById('payment-button-container');
+    const payButton = document.getElementById('rzp-button1');
+    payButton.className = 'btn btn-success';
+    payButton.innerText = `Checkout : ₹${afterGST.toFixed(2)}`;
+    
+    payButtonContainer.appendChild(payButton);
 }
+
+var show = printBasket();
 
 function printItem(pizza){
     const pTag = document.createElement('p');
-    pTag.innerText = `Pizza Name : ${pizza.name} Price : ${pizza.price}`;
+    pTag.innerText = `${pizza.name} ₹${pizza.price}`;
     return pTag;
 }
 
@@ -55,8 +83,8 @@ function createCard(pizza){
 
                 </div>
     */
-   const colDiv = document.createElement('div');
-   colDiv.className = 'col-4';
+    const colDiv = document.createElement('div');
+    colDiv.className = 'col-4';
     const cardDiv = document.createElement("div");
     cardDiv.className = 'card';
     cardDiv.style = {width:'18rem'};
